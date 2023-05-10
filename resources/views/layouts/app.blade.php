0c46e2f513af49db22d379a89a7e43ff278b636b
nicolo fossati <nicofossa01@gmail.com>
2023-05-10 15:21:07 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yeld('page-title')</title>
+</head>
+<body>
+    @include('header')
+    <header>
+        <h1>HEADER</h1>
+    </header>
+
+    <main>
+
+    </main>
+
+    
+    @include('header')
+    <footer>
+        <h1>FOOTER</h1>
+    </footer>
+</body>
+</html>
